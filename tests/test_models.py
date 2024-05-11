ec28a08c405ff68489af3c3d3b3c84bd313eacd6
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2024-05-11 10:54:56 -0400

    Add test_to_dict

--- tests/test_models.py
+++ tests/test_models.py
@@ -49,3 +49,17 @@ def test_from_dict(book_fixture):
     book2 = Book.from_dict(book2_dict)
 
     assert book1 == book2
+
+def test_to_dict(book_fixture):
+    book1 = book_fixture
+    book_dict = book1.to_dict()
+    expected =  {
+        "id": 1,
+        "title": "Yesterday's Tomorrows",
+        "short_title": "yesterdaystomorrows",
+        "author": "Wilfred Sinecure",
+        "url": "https://www.gutenberg.org/",
+        "filepath": None
+    }
+
+    assert book_dict == expected
